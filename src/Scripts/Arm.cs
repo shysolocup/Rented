3083a09083f36460030b912707a249c068633fa8
shysolocup <88659700+paishee@users.noreply.github.com>
2025-04-21 18:33:35 -0500

    guh

--- src/Scripts/Arm.cs
+++ src/Scripts/Arm.cs
@@ -28,30 +28,33 @@ public partial class Arm : Node3D
 		string[] spl = Delay.Split("/");
 		FloatingDelay = float.Parse(spl[0]) / float.Parse(spl[1]);
 
-		Camera ??= GetNode<Camera3D>("%PlayerCamera");
+		Camera ??= GetNode<Camera3D>("../%PlayerCamera");
 	}
 
 	public override void _Process(double delta)
 	{
-		Transform3D CameraGlobal = Camera.GlobalTransform;
-		Basis CameraBasis = CameraGlobal.Basis;
-		Vector3 Origin = CameraGlobal.Origin;
+		GD.Print(Camera);
+		if (Camera != null && IsInstanceValid(Camera)) {
+			Transform3D CameraGlobal = Camera.GlobalTransform;
+			Basis CameraBasis = CameraGlobal.Basis;
+			Vector3 Origin = CameraGlobal.Origin;
 
-		Vector3 ForwardVector = CameraBasis.Z;
-		Vector3 LeftVector = CameraBasis.X;
-		Vector3 UpVector = CameraBasis.Y;
+			Vector3 ForwardVector = CameraBasis.Z;
+			Vector3 LeftVector = CameraBasis.X;
+			Vector3 UpVector = CameraBasis.Y;
 
-		Origin += ForwardVector * -Distance;
-		Origin += LeftVector * 0.3f;
-		Origin += UpVector * -1f;
+			Origin += ForwardVector * -Distance;
+			Origin += LeftVector * 0.3f;
+			Origin += UpVector * -1f;
 
-		Origin = GlobalTransform.Origin.Lerp(Origin, this.FactorDelta(1/2f, delta));
+			Origin = GlobalTransform.Origin.Lerp(Origin, this.FactorDelta(1/2f, delta));
 
-		Basis ArmBasis = new(LeftVector, UpVector, ForwardVector);
-		ArmBasis = ArmBasis.Scaled(new Vector3(0.8f, 0.8f, 0.8f));
+			Basis ArmBasis = new(LeftVector, UpVector, ForwardVector);
+			ArmBasis = ArmBasis.Scaled(new Vector3(0.8f, 0.8f, 0.8f));
 
-		Transform3D ArmTransform = new(ArmBasis, Origin);
+			Transform3D ArmTransform = new(ArmBasis, Origin);
 
-		GlobalTransform = GlobalTransform.InterpolateWith(ArmTransform, this.FactorDelta(FloatingDelay, delta));
+			GlobalTransform = GlobalTransform.InterpolateWith(ArmTransform, this.FactorDelta(FloatingDelay, delta));	
+		}
 	}
 }
